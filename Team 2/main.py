9559bf02d376366d1b01958ee26544fa05343e51
Aditya-Bharadwaj <aditya21nov@gmail.com>
2018-05-09 20:56:39 +0530

    MatPlotLib Radar Graph

--- Team 2/main.py
+++ Team 2/main.py
@@ -3,6 +3,8 @@ import taster
 import json
 import os
 import utilities
+from math import pi
+import matplotlib.pyplot as plt
 
 
 parser = argparse.ArgumentParser(prog="Taste Profiling of food items")
@@ -25,12 +27,88 @@ parser.add_argument("-c",
                     )
 arguments = parser.parse_args()
 
+def show_graph(data):
+
+    # Set data
+    cat = list(data.keys())
+    values = list(data.values())
+
+    N = len(cat)
+
+    x_as = [n / float(N) * 2 * pi for n in range(N)]
+
+    # Because our chart will be circular we need to append a copy of the first 
+    # value of each list at the end of each list with data
+    values += values[:1]
+    x_as += x_as[:1]
+
+
+    # Set color of axes
+    plt.rc('axes', linewidth=0.5, edgecolor="#888888")
+
+
+    # Create polar plot
+    ax = plt.subplot(111, polar=True)
+
+
+    # Set clockwise rotation. That is:
+    ax.set_theta_offset(pi / 2)
+    ax.set_theta_direction(-1)
+
+
+    # Set position of y-labels
+    ax.set_rlabel_position(0)
+
+
+    # Set color and linestyle of grid
+    ax.xaxis.grid(True, color="#888888", linestyle='solid', linewidth=0.5)
+    ax.yaxis.grid(True, color="#888888", linestyle='solid', linewidth=0.5)
+
+
+    # Set number of radial axes and remove labels
+    plt.xticks(x_as[:-1], [])
+
+    # Set yticks
+    plt.yticks([2, 4, 6, 8, 10], ["2", "4", "6", "8", "10"])
+
+
+    # Plot data
+    ax.plot(x_as, values, linewidth=0, linestyle='solid', zorder=3)
+
+    # Fill area
+    ax.fill(x_as, values, 'b', alpha=0.3)
+
+
+    # Set axes limits
+    plt.ylim(0, 10)
+
+
+    # Draw ytick labels to make sure they fit properly
+    for i in range(N):
+        angle_rad = i / float(N) * 2 * pi
+
+        if angle_rad == 0:
+            ha, distance_ax = "center", 1
+        elif 0 < angle_rad < pi:
+            ha, distance_ax = "left", 1
+        elif angle_rad == pi:
+            ha, distance_ax = "center", 1
+        else:
+            ha, distance_ax = "right", 1
+
+        ax.text(angle_rad, 10 + distance_ax, cat[i], size=10, horizontalalignment=ha, verticalalignment="center")
+
+
+    # Show polar plot
+    plt.show()
+
 if arguments.profile:
     for dishfile in arguments.profile:
         for dish in utilities.read_json(dishfile):
             print("\n" + dish["dish_name"] + "\n" + "=" * len(dish["dish_name"]))
-            print(json.dumps(taster.taste(dish), sort_keys=True, indent="  "))
-
+            data = taster.taste(dish)
+            print(json.dumps(data, sort_keys=True, indent="  "))
+            show_graph(data)
 if arguments.validate:
     from validator import Validator
     adjustment = dict()
@@ -56,3 +134,6 @@ if arguments.classify:
             for item in sorted(value, key=lambda x: x[1], reverse=True):
                 print("{:14}: {:2}".format(item[0], round(item[1], 2)), end=' | ')
             print()
+
+
+
